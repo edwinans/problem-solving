a5324303bd8ae2591a72e059d7176e7b34284a0b
edwinans <edyansari5@gmail.com>
2023-12-04 00:49:37 +0100

    Refactor OCaml/partition.ml

--- OCaml/partition.ml
+++ OCaml/partition.ml
@@ -13,18 +13,16 @@ let subsets_sum s =
   let len = Array.length s in
   let max = (1 lsl len) - 1 in
   let mask = Array.make (max + 1) 0 in
-  for i = 0 to len - 1 do
-    mask.(1 lsl i) <- s.(i)
-  done;
   let sum = ref 0 in
-  for i = 0 to max do
-    sum := 0;
-    for j = 0 to len - 1 do
-      sum := !sum + mask.((1 lsl j) land i)
-    done;
-    mask.(i) <- !sum;
-  done;
-  mask
+  Array.iteri (fun i x -> mask.(1 lsl i) <- x) s;
+  Array.mapi (fun i _ ->
+      sum := 0;
+      for j = 0 to len - 1 do
+        sum := !sum + mask.((1 lsl j) land i)
+      done;
+      !sum
+    )
+    mask
 
 let is_partitionable s =
   let sums = subsets_sum s in
@@ -38,7 +36,7 @@ let is_partitionable s =
 
 
 let s = [|1;5;11;5|]
-let s = [|3;1;1;2;2;1|]
+let s = [|3;1;1;2;2;1;12|]
 
 let () =
   print_endline @@ string_of_bool @@ is_partitionable s
